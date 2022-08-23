234266d0144eadf934e48219e823c44d3b5cfcb9
jugaldb <jugalbhatt3@gmail.com>
2022-08-24 01:54:01 +0530

    fix: change time duration for cron

--- src/cron/youtube-video-get.cron.ts
+++ src/cron/youtube-video-get.cron.ts
@@ -1,6 +1,6 @@
 import cron from "node-cron";
 import { getData } from "../utils/youtube_data";
 
-cron.schedule("*/5 * * * * *", function () {
+cron.schedule("*/20 * * * * *", function () {
   getData("football");
 });
